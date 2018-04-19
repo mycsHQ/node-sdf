4ed2f685e3c6855ffd0f4c12a3768e728de30fb1
jroehl <mail@johannroehl.de>
2018-04-19 09:28:16 +0200

    fix typo

--- index.js
+++ index.js
@@ -12,36 +12,37 @@ const sdfcliPath = path.resolve(__dirname, '.dependencies', 'sdfcli');
  * @param {string} cmd
  * @param {string|object} options
  */
-async function handler(cmd, password = required('password'), options) {
-  if (!cliCommands.includes(cmd)) throw new Error(`Command "${ cmd }" not available in sdf cli`);
-  const args = options.entries(options).reduce((red, [ flag, value = '' ]) => `${ red }-${ flag } ${ value } `, '');
+function handler(cmd, password = required('password'), options) {
+  return new Promise((resolve, reject) => {
+    if (!cliCommands.includes(cmd)) throw new Error(`Command "${ cmd }" not available in sdf cli`);
+    const args = Object.entries(options).reduce((red, [ flag, value = '' ]) => `${ red }-${ flag } ${ value } `, '');
 
-  const sdfcli = spawn(sdfcliPath, [ cmd, args ]);
+    const sdfcli = spawn(sdfcliPath, [ cmd, args ]);
 
-  console.log(`\nExecuted Command:\n${ sdfcliPath } ${ cmd } ${ args }\n`);
+    console.log(`\nExecuted Command:\n${ sdfcliPath } ${ cmd } ${ args }\n`);
 
-  // send password to hidden prompt by sdfcli
-  sdfcli.stdin.write(`${ password }\n`);
+    // send password to hidden prompt by sdfcli
+    sdfcli.stdin.write(`${ password }\n`);
 
-  sdfcli.stdout.on('data', async data => {
-    const msg = data.toString();
-    // cleanup of duplicate "Enter password:" prompt
-    console.log(msg.replace('Enter password:', ''));
-    const lowerCaseMsg = msg.toLowerCase();
-    if (lowerCaseMsg.includes('?') || lowerCaseMsg.includes('Enter')) {
-      throw new Error('Only commands without prompts allowed!');
-    }
-  });
+    let res = '';
+    sdfcli.stdout.on('data', data => {
+      const msg = data.toString().replace('Enter password:', '');
+      // console.log(msg);
+      if (msg.match(/\?|enter/gi)) throw new Error('Only commands without prompts allowed!');
+      res += msg;
+    });
 
-  sdfcli.stderr.on('data', data => {
-    throw new Error(`>>> Error ${ data }`);
-  });
+    sdfcli.stderr.on('data', data => {
+      throw new Error(`>>> Error ${ data }`);
+    });
 
-  sdfcli.on('close', code => {
-    if (code !== 0) {
-      throw new Error(`>>> SDFCLI exited with code ${ code }`);
-    }
-    sdfcli.stdin.end();
+    sdfcli.on('close', code => {
+      if (code !== 0) {
+        throw new Error(`>>> SDFCLI exited with code ${ code }`);
+      }
+      sdfcli.stdin.end();
+      resolve(res);
+    });
   });
 }
 
