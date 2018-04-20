73863065afe1b805a34186e8cf3a22693059f66f
jroehl <mail@johannroehl.de>
2018-04-20 08:24:36 +0200

    add createProject methods

--- cli/sdf-create.js
+++ cli/sdf-create.js
@@ -1,41 +1,8 @@
 #!/usr/bin/env node
 const path = require('path');
-const fs = require('fs');
 
 const { prompt } = require('../lib/helpers');
-
-const {
-  netsuite: { account = '', email = '', url = '', role = 3 }
-} = require('../config');
-
-/**
- * Replace all occurences of placeholders in template specified as key/value pairs
- *
- * @param {object} replaceOptions object with keys to be replaced in template
- * @param {string} templatePath path to template
- *
- * @returns {string} modified template content
- */
-const processTemplate = (replaceOptions, templatePath) => {
-  const template = fs.readFileSync(path.join(__dirname, templatePath)).toString();
-  return Object.entries(replaceOptions).reduce((tmp, [ key, value ]) => tmp.replace(new RegExp(`@@${ key.toUpperCase() }`, 'g'), value), template);
-};
-
-/**
- * Make path in a recursive manner, creating all directories along the path
- *
- * @param {string} pathToCreate
- */
-const mkdirRecursive = pathToCreate => {
-  // Path separators could change depending on the platform
-  pathToCreate.split(path.sep).reduce((currentPath, folder) => {
-    currentPath += folder + path.sep;
-    if (!fs.existsSync(currentPath)) {
-      fs.mkdirSync(currentPath);
-    }
-    return currentPath;
-  }, '');
-};
+const { sdfCreateAccountCustomisationProject, sdfCreateSuiteAppProject } = require('../lib/methods');
 
 /**
  * Handle the create cli command
@@ -58,59 +25,22 @@ async function handler(cmd, { path: projectPath }) {
   `);
 
   const projectType = await prompt('Type 1 or 2 then press ENTER: ');
+  const projectName = await prompt('Enter a project name: ');
 
-  let publisherId;
-  let projectId;
-  let projectVersion;
-  let projectName;
-
-  if (projectType === '1') {
-    projectName = await prompt('Enter a project name: ');
-  } else if (projectType === '2') {
-    publisherId = await prompt('Enter your publisher ID: ');
-    projectId = await prompt('Enter your project ID: ');
-    projectName = await prompt('Enter your project name: ');
-    projectVersion = await prompt('Enter your project version: ');
-  } else {
-    console.error('Project type has to be either 1 or 2!');
-    process.exit(1);
+  switch (projectType) {
+      case '1':
+        return sdfCreateAccountCustomisationProject(projectName, projectPath);
+      case '2':
+        return sdfCreateSuiteAppProject(
+          projectName,
+          projectPath,
+          await prompt('Enter your publisher ID: '),
+          await prompt('Enter your project ID: '),
+          await prompt('Enter your project version: ')
+        );
+      default:
+        throw new Error('Project type has to be either 1 or 2!');
   }
-
-  let newProjectPath;
-  let customName;
-  if (projectType === '1') {
-    newProjectPath = path.resolve(projectPath, projectName);
-    mkdirRecursive(path.resolve(newProjectPath, 'FileCabinet', 'SuiteScripts', '.attributes'));
-  } else {
-    customName = `${ publisherId }.${ projectId }`;
-    newProjectPath = path.resolve(projectPath, customName);
-    mkdirRecursive(path.resolve(newProjectPath, 'SuiteApps', customName, '.attributes'));
-  }
-
-  mkdirRecursive(path.resolve(newProjectPath, 'Objects'));
-  fs.writeFileSync(path.resolve(newProjectPath, '.sdf'), `account=${ account }\nemail=${ email }\nrole=${ role }\nurl=${ url }`);
-
-  const replaceDeploy = {
-    path: projectType === '1' ? '~/FileCabinet/SuiteScripts/*' : `~/FileCabinet/SuiteApps/${ customName }/*`
-  };
-  const deployTemplate = processTemplate(replaceDeploy, './templates/deploy.xml');
-  fs.writeFileSync(path.resolve(newProjectPath, 'deploy.xml'), deployTemplate);
-
-  const replaceManifest = {
-    projectName: `<projectname>${ projectName }</projectname>`,
-    projectType: projectType === '1' ? 'ACCOUNTCUSTOMIZATION' : 'SUITEAPP',
-    publisherId: projectType === '2' ? `<publisherid>${ publisherId }</publisherid>` : '',
-    projectId: projectType === '2' ? `<projectid>${ projectId }</projectid>` : '',
-    projectVersion: projectType === '2' ? `<projectversion>${ projectVersion }</projectversion>` : ''
-  };
-  const manifestTemplate = processTemplate(replaceManifest, './templates/manifest.xml');
-  fs.writeFileSync(path.resolve(newProjectPath, 'manifest.xml'), manifestTemplate);
-
-  console.log(`
-  End of script.
-  Edit the .sdf file in the new project folder to complete the process
-  In the .sdf file, specify your account ID, login, and account URL.
-  `);
 }
 
 module.exports = handler;
--- config.js
+++ config.js
@@ -10,22 +10,54 @@ module.exports = {
     supplemental: 'https://system.netsuite.com/core/media/media.nl?id=87134768&c=NLCORP&h=8e8f2820ee2d719ac411&id=87134768&_xt=.gz&_xd=T&e=T.bin',
     maven: 'http://artfiles.org/apache.org/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz'
   },
-  cliCommands: [
-    'adddependencies',
-    'deploy',
-    'importbundle',
-    'importconfiguration',
-    'importfiles',
-    'importobjects',
-    'listbundles',
-    'listconfiguration',
-    'listfiles',
-    'listmissingdependencies',
-    'listobjects',
-    'preview',
-    'project',
-    'update',
-    'updatecustomrecordwithinstances',
-    'validate'
-  ]
+  cliCommands: {
+    adddependencies: 'adddependencies',
+    deploy: 'deploy',
+    importbundle: 'importbundle',
+    importconfiguration: 'importconfiguration',
+    importfiles: 'importfiles',
+    importobjects: 'importobjects',
+    listbundles: 'listbundles',
+    listconfiguration: 'listconfiguration',
+    listfiles: 'listfiles',
+    listmissingdependencies: 'listmissingdependencies',
+    listobjects: 'listobjects',
+    preview: 'preview',
+    project: 'project',
+    update: 'update',
+    updatecustomrecordwithinstances: 'updatecustomrecordwithinstances',
+    validate: 'validate'
+  },
+  templates: {
+    deploy: {
+      deploy: {
+        files: {},
+        objects: { path: '~/Objects/*' }
+      }
+    },
+    manifest: {
+      manifest: {
+        $: {},
+        frameworkversion: '1.0',
+        dependencies: {
+          features: {
+            feature: [
+              {
+                $: { required: 'true' },
+                _: 'CUSTOMRECORDS'
+              },
+              {
+                $: { required: 'true' },
+                _: 'SERVERSIDESCRIPTING'
+              },
+              {
+                $: { required: 'false' },
+                _: 'CREATESUITEBUNDLES'
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
 };
